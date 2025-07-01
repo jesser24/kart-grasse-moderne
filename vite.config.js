2b8d6bb2790aa79ba11252ae260df73e2df6987f
jesser24 <ka.jesser@epmistes.net>
2025-07-01 04:31:48 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/kart-grasse-moderne/', // <-- Ajoutez ou modifiez cette ligne
   plugins: [react(),tailwindcss()],
   resolve: {
     alias: {
